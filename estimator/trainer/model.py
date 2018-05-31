1d5b310e0b7504743e8971d48de1efa13872b2f4
Dev <devadattaj@gmail.com>
2018-05-31 22:56:48 +0530

    Update model.py

--- estimator/trainer/model.py
+++ estimator/trainer/model.py
@@ -224,15 +224,14 @@ NGTDM.Contrast,
 NGTDM.Busyness,
 NGTDM.Complexity,
 NGTDM.Strength,
-  ]
+]
 
 return tf.estimator.DNNLinearCombinedClassifier(
       config=config,
       linear_feature_columns=wide_columns,
       dnn_feature_columns=deep_columns,
       dnn_hidden_units=hidden_units or [100, 70, 50, 25]
-  )
-
+)
 
 def parse_label_column(label_string_tensor):
   """Parses a string tensor into the label tensor
